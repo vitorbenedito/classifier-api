584c5b252cc250b9ab61d380922379c34ef4ccd1
Vitor Nunes Benedito <vitorbenedito@Vitors-MacBook-Pro.local>
2017-07-22 11:26:26 -0300

    Translate

--- src/main/groovy/com/classifier/handler/TranslateText.groovy
+++ src/main/groovy/com/classifier/handler/TranslateText.groovy
@@ -1,5 +1,10 @@
 package com.classifier.handler
 
+import org.apache.commons.httpclient.util.URIUtil
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -9,7 +14,6 @@ import com.classifier.persistence.repository.ClassifierRepository
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Log4j
-import java.io.BufferedReader
 
 @Component
 @Log4j
@@ -35,40 +39,16 @@ class TranslateText implements Handler {
 	String text = classifier?.text?.trim()
 	String googleRequest = GOOGLE_TRANSLATE_URL + "/?key=" + GOOGLE_KEY + "&q=" + text + "&source=" + GOOGLE_DEFAULT_SOURCE + "&target=" + request.pathParameter('languageCode')
 	
-	URL url = new URL("http://g1.globo.com")
-	HttpURLConnection con = (HttpURLConnection) url.openConnection()
-  
-	
-	// By default it is GET request
-	con.setRequestMethod("GET")
-   
-	int responseCode = con.getResponseCode()
-	System.out.println("Sending get request : "+ url)
-	System.out.println("Response code : "+ responseCode)
-   
-	// Reading response from input Stream
-	 BufferedReader stream = new BufferedReader(new InputStreamReader(con.getInputStream()));
-	String output
-	StringBuffer response = new StringBuffer()
-   
-	while ((output = stream.readLine()) != null) {
-	 response.append(output);
-	}
-	stream.close();
-   
-	//printing result from response
-	System.out.println(response.toString());
-	
-//	HttpGet getRequest = new HttpGet(URIUtil.encodeQuery(googleRequest))
-//	HttpClient client = HttpClientBuilder.create().build();
-//	println "Request to google: " + googleRequest
-//	HttpResponse response = client.execute(getRequest);
-//	String translateText = response.getEntity().toString();
-//	println "Text: " + text
+	HttpGet getRequest = new HttpGet(URIUtil.encodeQuery(googleRequest))
+	HttpClient client = HttpClientBuilder.create().build();
+	println "Request to google: " + googleRequest
+	HttpResponse response = client.execute(getRequest);
+	String translateText = response.getEntity().toString();
+	println "Text: " + text
 	
     Response.builder()
         .statusCode(201)
-        .body(response.toString())
+        .body(translateText)
         .build()
   }
 
